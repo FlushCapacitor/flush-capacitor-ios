43f66cd32ecd0ed7296d71bde38edc4d1a31adba
Tomas Novella <tomasnovella@gmail.com>
2016-01-20 21:42:28 +0100

    Rewrite networking + (untested) honza's threads fix

--- flushForiOS/ToiletStatus.swift
+++ flushForiOS/ToiletStatus.swift
@@ -8,76 +8,52 @@
 
 import Foundation
 
-class ToiletStatus : NSObject, SRWebSocketDelegate {
-    let kWebSocketURL = "ws://itoilet/changes"
-    let kHTTPQueryURL = "http://itoilet/api/sensors" // @todo
-    
-    private var _statuses = [String:Bool]()
-    private var _webSocket: SRWebSocket?
-    
-    override init() {
-        super.init()
-        let host = NSURL(string: kWebSocketURL)?.host;
-        let _reachability = Reachability(hostName: host);
-        _reachability.startNotifier();
+func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithRequest(request){
+        (data, response, error) -> Void in
+        dispatch_async(dispatch_get_main_queue()) {
+            if error != nil {
+                callback("", error!.localizedDescription)
+            } else {
+                let result = NSString(data: data!, encoding:
+                    NSASCIIStringEncoding)!
+                callback(result as String, nil)
+            }
+        }
+    }
+    task.resume()
+}
 
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: "reachabilityChanged:",
-            name: kReachabilityChangedNotification,
-            object: nil);
-    }
-    
-    private func wsConnect() {
-        if(_webSocket == nil) {
-            _webSocket = SRWebSocket(URL: NSURL(string: kWebSocketURL))
-            _webSocket?.delegate = self
-            _webSocket?.open()
-        }
-    }
-    
-    private func wsDisconnect() {
-        if(_webSocket != nil) {
-            _webSocket?.close()
-            _webSocket = nil
-        }
-        _statuses.removeAll()
-    }
-    
-    func reachabilityChanged(notification:NSNotification) {
-        let r = notification.object as! Reachability
-        if(r.currentReachabilityStatus() == NetworkStatus.NotReachable) {
-            wsDisconnect()
-        } else {
-            wsConnect()
-        }
-    }
-    
-    // MARK: SRWebSocketDelegate
-    func webSocketDidOpen(webSocket: SRWebSocket!) {
-    }
-    
-    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
-        if let msg = (try? NSJSONSerialization.JSONObjectWithData(
-            message.dataUsingEncoding(NSUTF8StringEncoding)!, options:[])) as? NSDictionary
-        {
-            let locked = msg["state"]?.isEqualToString("locked")
-            let name = msg["name"] as! String
-            _statuses[name] = locked
-        }
-    }
-    
-    func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: Int, reason: String!, wasClean: Bool) {
-        wsDisconnect()
-    }
-    
-    func webSocket(webSocket: SRWebSocket!, didFailWithError error: NSError!) {
-        wsDisconnect()
-    }
+class ToiletStatus /*: NSObject, SRWebSocketDelegate */{
+    let kWebSocketURL = "ws://itoilet/changes"
+    let kHTTPQueryURL = "http://itoilet/api/sensors"
     
     //MARK public API
-    func getStatuses() -> [String:Bool] {
-        return _statuses
+    func updateStatuses(callback: [String:Bool]->Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: kHTTPQueryURL)!)
+        
+        httpGet(request) { (data, error) -> Void in
+            
+            var _statuses = [String:Bool]()
+            if error != nil {
+                _statuses.removeAll()
+            } else {
+                let jsonizedMessage = try? NSJSONSerialization.JSONObjectWithData(data.dataUsingEncoding(NSUTF8StringEncoding)!, options:[])
+                guard let responseJson = jsonizedMessage as? [[String:String]] else {
+                    return
+                }
+                
+                // example: toilet = {"name": "L", state: "unlocked"}
+                for toilet in responseJson {
+                    _statuses[toilet["name"]!] = toilet["state"]! == "unlocked"
+                }
+            }
+            callback(_statuses)
+            //callback(["L": false, "R": true])
+            print("@@ \(_statuses)")
+        }
     }
+    
     
 }
